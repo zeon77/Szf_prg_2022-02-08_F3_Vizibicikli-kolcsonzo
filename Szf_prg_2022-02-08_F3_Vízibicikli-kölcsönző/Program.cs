dd55072bf15b1c1adf398a214ddb677ef897f151
zeon77 <2716313+zeon77@users.noreply.github.com>
2022-05-12 09:08:02 +0200

    Add project files.

---
+++ Szf_prg_2022-02-08_F3_Vízibicikli-kölcsönző/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Szf_prg_2022_02_08_F3_Vízibicikli_kölcsönző
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

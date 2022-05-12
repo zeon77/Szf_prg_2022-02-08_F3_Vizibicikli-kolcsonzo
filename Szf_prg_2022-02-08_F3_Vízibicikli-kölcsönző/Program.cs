369f44e9e99882110a8e7fbc783b861a7ee3e426
zeon77 <2716313+zeon77@users.noreply.github.com>
2022-05-12 09:52:43 +0200

    6.

--- Szf_prg_2022-02-08_F3_Vízibicikli-kölcsönző/Program.cs
+++ Szf_prg_2022-02-08_F3_Vízibicikli-kölcsönző/Program.cs
@@ -18,6 +18,22 @@ namespace VizibicikliKolcsonzo
 
             //5.
             Console.WriteLine($"5. feladat: Napi kölcsönzések száma: {kolcsonzesek.Count}");
+
+            //6.
+            Console.Write($"6. feladat: Kérek egy nevet: ");
+            string név = Console.ReadLine();
+
+            var list = kolcsonzesek.Where(k => k.Név == név).ToList();
+            if (list.Count == 0)
+            {
+                Console.WriteLine($"Nem volt ilyen nevű kölcsönző...");
+            }
+            else
+            {
+                Console.WriteLine($"\t{név} kölcsönzései:");
+                list.ForEach(k => Console.WriteLine($"\t{k.Tól.ToString(@"hh\:mm")}-{k.Ig.ToString(@"hh\:mm")}"));
+            }
+
         }
     }
 }
